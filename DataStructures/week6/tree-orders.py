7534e0b3faa27d8e7c86e780763e193ef5e95f98
Abe Vallerian <abe.vallerian@gmail.com>
2024-03-24 22:33:37 +0700

    optimize script

--- DataStructures/week6/tree-orders.py
+++ DataStructures/week6/tree-orders.py
@@ -1,69 +1,92 @@
 import sys
 import threading
-from typing import List
+from typing import Dict, List
 
-sys.setrecursionlimit(10**6)  # max depth of recursion
-threading.stack_size(2**27)  # new thread will get stack of such size
+sys.setrecursionlimit(10**7)  # max depth of recursion
+threading.stack_size(2**28)  # new thread will get stack of such size
 
 
 class TreeOrders:
     def read(self):
         self.n: int = int(sys.stdin.readline())
-        self.key: List[int] = [0 for _ in range(self.n)]
-        self.left: List[int] = [0 for _ in range(self.n)]
-        self.right: List[int] = [0 for _ in range(self.n)]
+        self.key: Dict[int, int] = {}
+        self.left: Dict[int, int] = {}
+        self.right: Dict[int, int] = {}
         for i in range(self.n):
             [a, b, c] = map(int, sys.stdin.readline().split())
             self.key[i] = a
             self.left[i] = b
             self.right[i] = c
 
-    def inOrder(self, root_idx: int = 0) -> List[int]:
-        if self.right[root_idx] == -1 and self.right[root_idx] == -1:
-            return [self.key[root_idx]]
-        elif self.left[root_idx] == -1:
-            return [self.key[root_idx]] + self.inOrder(self.right[root_idx])
-        elif self.right[root_idx] == -1:
-            return self.inOrder(self.left[root_idx]) + [self.key[root_idx]]
-        return (
-            self.inOrder(self.left[root_idx])
-            + [self.key[root_idx]]
-            + self.inOrder(self.right[root_idx])
-        )
+    def inOrder(self, root_idx: int = 0) -> None:
+        left: int = self.left[root_idx]
+        right: int = self.right[root_idx]
+        key: int = self.key[root_idx]
 
-    def preOrder(self, root_idx: int = 0) -> List[int]:
-        if self.left[root_idx] == -1 and self.right[root_idx] == -1:
-            return [self.key[root_idx]]
-        elif self.left[root_idx] == -1:
-            return [self.key[root_idx]] + self.preOrder(self.right[root_idx])
-        elif self.right[root_idx] == -1:
-            return [self.key[root_idx]] + self.preOrder(self.left[root_idx])
-        return (
-            [self.key[root_idx]]
-            + self.preOrder(self.left[root_idx])
-            + self.preOrder(self.right[root_idx])
-        )
+        if left != -1:
+            self.inOrder(left)
+        print(key, end=" ")
+        if right != -1:
+            self.inOrder(right)
+        # if left == -1 and right == -1:
+        #     return [key]
+        # elif left == -1:
+        #     return [key] + self.inOrder(right)
+        # elif right == -1:
+        #     return self.inOrder(left) + [key]
+        # return self.inOrder(left) + [key] + self.inOrder(right)
 
-    def postOrder(self, root_idx: int = 0) -> List[int]:
-        if self.left[root_idx] == -1 and self.right[root_idx] == -1:
-            return [self.key[root_idx]]
-        elif self.left[root_idx] == -1:
-            return self.postOrder(self.right[root_idx]) + [self.key[root_idx]]
-        elif self.right[root_idx] == -1:
-            return self.postOrder(self.left[root_idx]) + [self.key[root_idx]]
-        return (
-            self.postOrder(self.left[root_idx])
-            + self.postOrder(self.right[root_idx])
-            + [self.key[root_idx]]
-        )
+    def preOrder(self, root_idx: int = 0) -> None:
+        left: int = self.left[root_idx]
+        right: int = self.right[root_idx]
+        key: int = self.key[root_idx]
+
+        print(key, end=" ")
+        if left != -1:
+            self.preOrder(left)
+        if right != -1:
+            self.preOrder(right)
+
+        # if left == -1 and right == -1:
+        #     return [key]
+        # elif left == -1:
+        #     return [key] + self.preOrder(right)
+        # elif right == -1:
+        #     return [key] + self.preOrder(left)
+        # return [key] + self.preOrder(left) + self.preOrder(right)
+
+    def postOrder(self, root_idx: int = 0) -> None:
+        left: int = self.left[root_idx]
+        right: int = self.right[root_idx]
+        key: int = self.key[root_idx]
+
+        if left != -1:
+            self.postOrder(left)
+        if right != -1:
+            self.postOrder(right)
+        print(key, end=" ")
+
+        # if left == -1 and right == -1:
+        #     return [key]
+        # elif left == -1:
+        #     return self.postOrder(right) + [key]
+        # elif right == -1:
+        #     return self.postOrder(left) + [key]
+        # return self.postOrder(left) + self.postOrder(right) + [key]
 
 
 def main() -> None:
     tree: TreeOrders = TreeOrders()
     tree.read()
-    print(" ".join(str(x) for x in tree.inOrder()))
-    print(" ".join(str(x) for x in tree.preOrder()))
-    print(" ".join(str(x) for x in tree.postOrder()))
+    # print(" ".join(str(x) for x in tree.inOrder()))
+    # print(" ".join(str(x) for x in tree.preOrder()))
+    # print(" ".join(str(x) for x in tree.postOrder()))
+
+    tree.inOrder()
+    print()
+    tree.preOrder()
+    print()
+    tree.postOrder()
 
 
 threading.Thread(target=main).start()
